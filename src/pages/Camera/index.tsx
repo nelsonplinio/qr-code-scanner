54d04f6306d813c93467da677842c458ee9fdf51
Nelson Plíni <nelsonplinio94@gmail.com>
2020-07-31 15:43:41 -0300

    Feat: Scanner qr code

---
+++ src/pages/Camera/index.tsx
@@ -0,0 +1,194 @@
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useRef,
+  useMemo,
+} from 'react';
+import { Feather } from '@expo/vector-icons';
+import { openURL, canOpenURL } from 'expo-linking';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Share, Alert, Dimensions, Clipboard } from 'react-native';
+import { Modalize } from 'react-native-modalize';
+
+import {
+  CameraExpo,
+  Container,
+  ModalContainer,
+  ModalTitle,
+  TextScanned,
+  ActionsContainer,
+  ActionButton,
+  ActionButtonText,
+  AccessDeniedContainer,
+  AccessDeniedText,
+  AccessDeniedDescriptionText,
+  RequestPermissionButton,
+  RequestPermissionButtonText,
+} from './styles';
+
+interface BarCodeScanned {
+  data: string;
+  type: number;
+}
+
+interface LinkScanned {
+  data: string;
+  canOpen: boolean;
+  date: Date;
+}
+
+const Camera: React.FC = () => {
+  const modalRef = useRef<Modalize>(null);
+  const [dataScanned, setDataScanned] = useState<LinkScanned>(
+    {} as LinkScanned,
+  );
+  const [scanned, setScanned] = useState(false);
+  const [linkCopied, setLinkCopied] = useState(false);
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+
+  const modalHeight = useMemo(() => {
+    const { height } = Dimensions.get('screen');
+
+    return height * 0.65;
+  }, []);
+
+  const modalSnapPoint = useMemo(() => {
+    const { height } = Dimensions.get('screen');
+
+    return height * 0.45;
+  }, []);
+
+  const handleRequestPermission = useCallback(async () => {
+    const permission = await CameraExpo.getPermissionsAsync();
+
+    if (!permission.canAskAgain || permission.granted) {
+      setHasCameraPermission(true);
+      return;
+    }
+
+    const { granted } = await CameraExpo.requestPermissionsAsync();
+
+    setHasCameraPermission(granted);
+  }, []);
+
+  const handleOnBarCodeScanned = useCallback(
+    async ({ data }: BarCodeScanned) => {
+      setScanned(true);
+
+      const canOpen = await canOpenURL(data);
+
+      setDataScanned({
+        data,
+        canOpen,
+        date: new Date(),
+      });
+
+      modalRef.current?.open();
+    },
+    [],
+  );
+
+  const handleShareLink = useCallback(async () => {
+    try {
+      await Share.share({
+        message: dataScanned.data,
+      });
+    } catch (error) {
+      Alert.alert('Não foi possivel compartilhar esse link');
+    }
+  }, [dataScanned]);
+
+  const handleOpenLink = useCallback(() => {
+    openURL(dataScanned.data);
+  }, [dataScanned]);
+
+  const handleClipboardLink = useCallback(() => {
+    Clipboard.setString(dataScanned.data);
+    setLinkCopied(true);
+  }, [dataScanned]);
+
+  const handleModalClosed = useCallback(() => {
+    setScanned(false);
+    setLinkCopied(false);
+  }, []);
+
+  useEffect(() => {
+    handleRequestPermission();
+  }, [handleRequestPermission]);
+
+  if (!hasCameraPermission) {
+    return (
+      <AccessDeniedContainer>
+        <Feather name="camera-off" size={78} color="#999" />
+
+        <AccessDeniedText>Precisamos do acesso da camera</AccessDeniedText>
+
+        <AccessDeniedDescriptionText>
+          Precisamos do acesso a camera, para que possamos scanear os QRCodes.
+        </AccessDeniedDescriptionText>
+
+        <RequestPermissionButton onPress={handleRequestPermission}>
+          <RequestPermissionButtonText>
+            PERMITIR ACESSO À CAMERA
+          </RequestPermissionButtonText>
+        </RequestPermissionButton>
+      </AccessDeniedContainer>
+    );
+  }
+
+  return (
+    <Container>
+      <CameraExpo
+        type={CameraExpo.Constants.Type.back}
+        barCodeScannerSettings={{
+          barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
+        }}
+        onBarCodeScanned={scanned ? undefined : handleOnBarCodeScanned}
+        ratio="16:9"
+      />
+
+      <Modalize
+        ref={modalRef}
+        modalHeight={modalHeight}
+        snapPoint={modalSnapPoint}
+        onClosed={handleModalClosed}
+      >
+        <ModalContainer>
+          <ModalTitle>Contaudo scaneado</ModalTitle>
+
+          <TextScanned>{dataScanned.data}</TextScanned>
+
+          <ActionsContainer>
+            {dataScanned.canOpen && (
+              <ActionButton onPress={handleOpenLink}>
+                <Feather name="external-link" size={24} color="#666" />
+                <ActionButtonText>Abrir conteudo</ActionButtonText>
+              </ActionButton>
+            )}
+
+            <ActionButton onPress={handleClipboardLink}>
+              <Feather name="copy" size={24} color="#666" />
+              <ActionButtonText>
+                Copiar conteudo
+                {linkCopied && <ActionButtonText> (Copiado)</ActionButtonText>}
+              </ActionButtonText>
+            </ActionButton>
+
+            <ActionButton onPress={handleShareLink}>
+              <Feather name="share" size={24} color="#666" />
+              <ActionButtonText>Compartilhar conteudo</ActionButtonText>
+            </ActionButton>
+
+            <ActionButton>
+              <Feather name="save" size={24} color="#666" />
+              <ActionButtonText>Salvar conteudo</ActionButtonText>
+            </ActionButton>
+          </ActionsContainer>
+        </ModalContainer>
+      </Modalize>
+    </Container>
+  );
+};
+
+export default React.memo(Camera);
---
+++ src/pages/Camera/styles.ts
@@ -0,0 +1,92 @@
+import styled from 'styled-components/native';
+import { Camera, CameraProps } from 'expo-camera';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const CameraExpo = styled(Camera)<CameraProps>`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+export const ModalContainer = styled.View`
+  padding: 16px;
+`;
+
+export const ModalTitle = styled.Text`
+  font-size: 32px;
+`;
+
+export const TextScanned = styled.Text`
+  border: solid 1px rgba(100, 100, 100, 0.5);
+  border-radius: 10px;
+  padding: 16px 8px;
+  margin-top: 22px;
+`;
+
+export const ActionsContainer = styled.View`
+  margin-top: 16px;
+`;
+
+export const ActionButton = styled(RectButton)`
+  flex-direction: row;
+  align-items: baseline;
+  padding: 16px 0;
+`;
+
+export const ActionButtonText = styled.Text`
+  margin-left: 16px;
+  font-size: 18px;
+  color: #666;
+  margin-right: auto;
+`;
+
+export const AccessDeniedContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background: #000;
+  padding: 22px;
+`;
+
+export const AccessDeniedText = styled.Text`
+  color: #999;
+  width: 100%;
+  text-align: center;
+  margin-top: 22px;
+  font-size: 24px;
+  font-weight: bold;
+  margin: 28px 0 16px;
+  flex-direction: column;
+`;
+
+export const AccessDeniedDescriptionText = styled(AccessDeniedText)`
+  font-size: 18px;
+  margin-top: 6px;
+  opacity: 0.7;
+`;
+
+export const RequestPermissionButton = styled(RectButton)`
+  background: #365df0;
+  border-radius: 10px;
+  height: 55px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  margin-top: 16px;
+`;
+
+export const RequestPermissionButtonText = styled.Text`
+  width: 100%;
+  height: 100%;
+
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+`;
